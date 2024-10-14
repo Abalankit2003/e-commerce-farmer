41f1fa5382e800b997a4bfaf3d6c46deca8b5d20
Ankit Raj <ankitraj90747@gmail.com>
2024-10-14 22:11:59 +0530

    connected shopping component to backend

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Card({props}) {
   return (
-    <div className="border w-[280px] h-[300px] rounded-lg shadow-md hover:shadow-lg bg-white flex flex-col">
+    <div className="border w-[280px] h-[300px] rounded-lg shadow-md hover:shadow-lg hover:bg-slate-200 bg-white flex flex-col">
       <img src={props.imageUrl} alt={props.name} className="h-3/5 object-cover overflow-hidden rounded-lg" />
       <div className='p-5'>
         <p className='text-center'>{props.name}</p>
--- client/src/pages/Shop.jsx
+++ client/src/pages/Shop.jsx
@@ -1,12 +1,24 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {FaArrowLeft, FaArrowRight} from 'react-icons/fa6';
 import Header from '../components/Header';
-import { items, products } from '../constants/shopping';
 import Card from '../components/Card';
 
 function Shop() {
 
   const [currItem, setCurrItem] = useState(0);
+  const [allProducts, setAllProducts] = useState([]);
+
+  useEffect(() => {
+    // Immediately Invoked Async Function(IIFE) => declares and immediately invokes the async function.
+    // (async () => {})() => Syntax of IIFE
+
+    (async () => {
+      const res = await fetch("/api/shopping/products");
+
+      const data = await res.json();
+      setAllProducts(data);
+    })();
+  }, []);
 
   const handleleftArrow = () => {
     let total = items.length;
@@ -21,28 +33,35 @@ function Shop() {
   return (
     <div className="flex flex-col gap-1 bg-gray-200">
       <Header title="Shopping" />
-      <div className="relative flex items-center justify-center box-border h-[320px]">
-        <img
-          src={items[currItem].imageUrl}
-          alt={items[currItem].name}
-          className="flex-1 h-full object-cover"
-        />
+      {allProducts.length > 0 && (
+        <div className="relative flex items-center justify-center box-border h-[320px]">
+          <img
+            src={allProducts[currItem].productImages[0]}
+            alt={allProducts[currItem].productName}
+            className="flex-1 h-full object-cover"
+          />
 
-        <FaArrowLeft
-          className="absolute left-4 text-white text-3xl cursor-pointer"
-          onClick={handleleftArrow}
-        />
-        <FaArrowRight
-          className="absolute right-4 text-white text-3xl cursor-pointer"
-          onClick={handleRightArrow}
-        />
-      </div>
-      <div className='p-3 flex flex-row flex-wrap gap-5'>
-        {products && 
-          products.map(product => (
-            <Card props = {product}/>
-          ))
-        }
+          <FaArrowLeft
+            className="absolute left-4 text-slate-700 text-3xl cursor-pointer"
+            onClick={handleleftArrow}
+          />
+          <FaArrowRight
+            className="absolute right-4 text-slate-700 text-3xl cursor-pointer"
+            onClick={handleRightArrow}
+          />
+        </div>
+      )}
+      <div className="p-3 flex flex-row flex-wrap gap-5">
+        {allProducts &&
+          allProducts.map((product) => (
+            <Card
+              props={{
+                name: product.productName,
+                imageUrl: product.productImages[0],
+                price: product.productPrice,
+              }}
+            />
+          ))}
       </div>
     </div>
   );
